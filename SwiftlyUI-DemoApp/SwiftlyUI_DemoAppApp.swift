186839a1a8f2ba001883008a068be3511bcf7ca2
Ben Van Aken <ben.van.aken@icloud.com>
2025-11-05 19:53:22 +0800

    Initial Commit

---
+++ SwiftlyUI-DemoApp/SwiftlyUI_DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftlyUI_DemoAppApp.swift
+//  SwiftlyUI-DemoApp
+//
+//  Created by Ben Van Aken on 05/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftlyUI_DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
